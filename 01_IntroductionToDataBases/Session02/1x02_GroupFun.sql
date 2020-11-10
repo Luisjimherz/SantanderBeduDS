ed5c92509403f04a82e0ebe88e33bd50cecfbb23
Luis Fer Jiménez <66329525+Luisjimherz@users.noreply.github.com>
2020-11-10 15:34:01 -0600

    Session 2 updating

--- 01_IntroductionToDataBases/Session02/1x02_GroupFun.sql
+++ 01_IntroductionToDataBases/Session02/1x02_GroupFun.sql
@@ -1,11 +1,3 @@
-# Filtrado con expresiones regulares
-USE tienda;
-SHOW tables;
-SELECT * FROM empleado;
-SELECT *  FROM empleado WHERE nombre LIKE 'M%';
-SELECT * FROM empleado WHERE nombre LIKE '%a';
-SELECT * FROM empleado WHERE nombre LIKE 'M%l_a';
-
 # Funciones de agregación
 SELECT (1+7) AS pipi; # AS asigna un identificador de campo a una expresión
 SELECT * FROM articulo;
