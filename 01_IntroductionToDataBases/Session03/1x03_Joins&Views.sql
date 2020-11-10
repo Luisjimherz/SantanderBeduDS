e9114a95ec095d0527c9eec3223c5cf4a6587f8f
Luis Fer Jiménez <66329525+Luisjimherz@users.noreply.github.com>
2020-11-10 16:02:21 -0600

    Work from session 3 splited

--- 01_IntroductionToDataBases/Session03/1x03_Joins&Views.sql
+++ 01_IntroductionToDataBases/Session03/1x03_Joins&Views.sql
@@ -1,16 +1,3 @@
-### Subconsultas ###
-# Subconsulta en WHERE
-# Nombre de los empleados cuyo puesto es developer
-SELECT nombre, apellido_paterno 
-FROM empleado 
-WHERE id_puesto IN (SELECT id_puesto FROM puesto WHERE nombre LIKE '%developer%');
-
-# Subconsulta en FROM
-# menor y mayor cantidad de ventas de un artículo
-SELECT id_articulo, MIN(cuentaArticulo), MAX(cuentaArticulo) 
-FROM (SELECT COUNT(*) AS cuentaArticulo FROM venta) AS subconsulta  
-GROUP BY clave, id_articulo;
-
 ### JOINS
 SHOW KEYS FROM venta;
 SELECT * FROM puesto;
